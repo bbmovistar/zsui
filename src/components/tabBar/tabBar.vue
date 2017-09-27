ca6f228f9cdcf1b7785d68ca5156a57bcf453d0c
bbmovistar <bbmovistar@sina.com>
2017-09-27 17:38:36 +0800

    修改tabbar

--- src/components/tabBar/tabBar.vue
+++ src/components/tabBar/tabBar.vue
@@ -1,9 +1,8 @@
 <template>
-    <div class="tabbar">
+    <div class="zs-tabbar">
         <div class="tabs">
             <div class="tab " :class="{ 'active' : item.click === true}" v-for="(item,index) in tab" :key="item.value" @click="change(item)" >
                 {{item.label}}
-                {{item.click}}
             </div>
         </div>
     </div>
